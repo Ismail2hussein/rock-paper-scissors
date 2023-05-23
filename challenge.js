d0ce57226b3c13bf69f0623ef95ec0e3c0a6e670
Ismail Hussein <133203884+Ismail2hussein@users.noreply.github.com>
2023-05-23 07:13:44 -0700

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,22 +1,17 @@
-const playerChoices = ["rock", "paper", "scissors"];
+function playGame(player3, player4) {
+  if (player3 === player4) {
+    return "tie";
+  } else if (
+    (player3 === "rock" && player4 === "scissors") ||
+    (player3 === "paper" && player4 === "rock") ||
+    (player3 === "scissors" && player4 === "paper")
+  ) {
+    return "player3 wins";
+  } else {
+    return "player4 wins";
+  }
+}
 
-    // Function to generate random choice for computer opponent
-    function getRandomChoice() {
-      let index = Math.floor(Math.random() * 3);
-      return playerChoices[index];
-    }
 
-    // Main logic of the game
-    function playGame(choice1, choice2) {
-      let result;
-
-      switch (true) {
-        case (choice1 === 'rock' && choice2 ==='scissors'):
-          result = `${choice1} defeats ${choice2}, you win!`;
-          break;
-
-        case (choice1 === 'rock' && choice2 === 'paper'):
-          result = `${choice1} defeats ${choice2}, you win!`;
-          break;
-      }
-    }
+console.log(playGame("rock", "paper")); // Output: "player4 wins"
+console.log(playGame("scissors", "scissors")); // Output: "tie"

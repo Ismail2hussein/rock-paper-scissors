75e2a58e8f1c748596accc9047f2b68158fa28a1
Ismail2hussein <ihusseinmohamed629@gmail.com>
2023-05-23 17:09:19 -0700

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,2 +1,22 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+const playerChoices = ["rock", "paper", "scissors"];
+
+    // Function to generate random choice for computer opponent
+    function getRandomChoice() {
+      let index = Math.floor(Math.random() * 3);
+      return playerChoices[index];
+    }
+
+    // Main logic of the game
+    function playGame(choice1, choice2) {
+      let result;
+
+      switch (true) {
+        case (choice1 === 'rock' && choice2 ==='scissors'):
+          result = `${choice1} defeats ${choice2}, you win!`;
+          break;
+
+        case (choice1 === 'rock' && choice2 === 'paper'):
+          result = `${choice1} defeats ${choice2}, you win!`;
+          break;
+      }
+    }
